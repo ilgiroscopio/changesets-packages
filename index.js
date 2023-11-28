0466ba0c3d670bef2cf32e58ff20941e1b6bf584
bertuccellimatteo <bertuccellimatteo@outlook.it>
2023-11-28 16:35:09 +0100

    fix: json parse the inputs and check if they are arrays

--- index.js
+++ index.js
@@ -3,7 +3,16 @@ const core = require("@actions/core");
 const changesetsPublishedPackages = JSON.parse(
   core.getInput("changesets-action-published-packages", { required: true }),
 );
-const packages = core.getInput("packages");
+if (!Array.isArray(changesetsPublishedPackages)) {
+  throw new Error(
+    "'changesets-action-published-packages' input is not a JSON array",
+  );
+}
+
+const packages = JSON.parse(core.getInput("packages"), { required: true });
+if (!Array.isArray(packages)) {
+  throw new Error("'packages' input is not a JSON array");
+}
 
 const parsedPackages = packages
   .map(package => {
